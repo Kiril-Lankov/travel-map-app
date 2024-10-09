2de60121f76c2b82c492941a48c38b3c9aea3ad1
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-09 21:21:08 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const pinRoute = require("./routes/pins")
 
 const app = express();
 
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGO_URL).then(()=> {
 .catch((error)=> {
     console.log(error)
 });
+
+app.use("/api/pins", pinRoute);
 
 app.listen(8800, ()=> {
     console.log("Backend Server is running!")
