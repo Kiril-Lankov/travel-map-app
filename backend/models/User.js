6f99c6110ee304e98044ed1d688ea0fdcb8f8050
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-09 21:22:56 +0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,41 +1,27 @@
 const mongoose = require("mongoose");
-const { type } = require("os");
 
 const UserSchema = new mongoose.Schema(
-    {
-        username: {
-            type: String,
-            require:true,
-            
-        },
-        title: {
-            type:String,
-            require: true,
-            min: 3,
-
-        },
-        password: {
-            type:String,
-            required: true,
-            min: 6,
-        },
-
-        rating: {
-            type: Number,
-            required: true,
-            min: 0,
-            max: 5
-        },
-        lat: {
-            type: Number,
-            require: true
-        },
-        long: {
-            type: Number,
-            require: true
-        }
+  {
+    username: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 20,
+      unique: true,
     },
-    {timestamps: true}
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("User", UserSchema);
