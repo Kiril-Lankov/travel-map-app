0ac9fb022793ca284ec8f5970c0c0110455ecf82
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-09 20:39:40 +0300

    UserSchema created in User.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(()=> {
+    console.log("MongoDB Connected!")
+})
+.catch((error)=> {
+    console.log(error)
+});
 
 app.listen(8800, ()=> {
     console.log("Backend Server is running!")
