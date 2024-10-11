64cffce58533da4420a575a293bd6d0faaa0af9a
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-11 15:09:48 +0300

    MapBOX component added to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-
+import * as React from 'react';
+import Map from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function App() {
+
   return (
     <div className="App">
-      Hello
+  <Map
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{width: 600, height: 400}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
     </div>
   );
 }
