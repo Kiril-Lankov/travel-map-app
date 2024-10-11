829efaf639af6d72d2292d4e19052d99bf7e570c
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-11 11:35:05 +0300

    index.js updated userRoute added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const pinRoute = require("./routes/pins")
+const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
 
 const app = express();
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=> {
 });
 
 app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800, ()=> {
     console.log("Backend Server is running!")
