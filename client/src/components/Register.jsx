32277e3b9c4c29ba10761d9a51b79402eb1c3c52
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-13 16:23:18 +0300

    Update Register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,20 +1,40 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./register.css";
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 export default function Register() {
     const [success, setSuccess] = useState(false);
     const [error, setError] = useState(false);
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const handleSubmit = async (e) => {
+e.preventDefault();
+const newUser = {
+    username: nameRef.current.value,
+    email: emailRef.current.value,
+    password: passwordRef.current.value,
+};
+
+try {
+  await axios.post("/api/users", newUser);
+  setError(false)
+  setSuccess(true)
+} catch (error) {
+    console.log(error)
+}
+    }
     return (
         <div className="registerContainer">
             <div className="logo">
                 <LocationOnIcon/>
                 KiToPin
                 </div>
-                <form>
-                    <input type="text" placeholder="username"/>
-                    <input type="email" placeholder="email" />
-                    <input type="password" placeholder="password" />
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="username" ref={nameRef}/>
+                    <input type="email" placeholder="email" ref={emailRef} />
+                    <input type="password" placeholder="password" ref={passwordRef}/>
                     <button className="registerBtn">Register</button>
                     {success &&
                     <span className="success">Successful.You can login now!</span>}
