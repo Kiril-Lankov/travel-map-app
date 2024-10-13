c33b1a070f05fc3fd92b603babc4e78bb8bc94aa
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-13 15:10:54 +0300

    App.js updated and app.css too

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/users");
 const pinRoute = require("./routes/pins");
 
 dotenv.config();
+
+app.use(cors());
 
 app.use(express.json());
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,81 +1,164 @@
-import * as React from 'react';
-import Map, {Marker, Popup} from 'react-map-gl';
+import React, { useState, useEffect } from 'react';
+import Map, { Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { useState ,useEffect} from 'react';
 import StarIcon from '@mui/icons-material/Star';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-import "./app.css";
-import axios from "axios";
-import {format} from "timeago.js";
-
-
+import './app.css';
+import axios from 'axios';
+import { format } from 'timeago.js';
 
 function App() {
-  const [pins, setPins] = useState([])
+  const [pins, setPins] = useState([]);
   const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [newPlace, setNewPlace] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [rating, setRating] = useState(0);
+  const [viewState, setViewState] = useState({
+    longitude: 15,
+    latitude: 47,
+    zoom: 4
+  });
+  const currentUser = 'Gery';
 
-useEffect(()=> {
-  const getPins = async () => {
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get('/api/pins');
+        setPins(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPins();
+  }, []);
+
+  // When a marker is clicked, update viewState to move the map to that marker
+  const handleMarkerClick = (lat, long, id) => {
+    setCurrentPlaceId(id);
+    setViewState({
+      ...viewState,
+      longitude: long,
+      latitude: lat,
+      zoom: 8,
+      transitionDuration: 'flyTo'
+    });
+  };
+
+  const handleAddClick = (e) => {
+    const lat = e.lngLat.lat;
+    const long = e.lngLat.lng;
+    setNewPlace({
+      lat,
+      long
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating,
+      long: newPlace.long,
+      lat: newPlace.lat,
+      
+    }
+    console.log(newPin)
     try {
-      const res = await axios.get("/api/pins");
-      setPins(res.data);
+      const res = await axios.post("/api/pins", newPin);
+      setPins([...pins, res.data]);
+      setNewPlace(null);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
-  getPins();
-},[]);
 
-const handleMarkerClick = (id) => {
-  console.log("Marker clicked: ", id);
-  setCurrentPlaceId(id)
-}
+  
   return (
     <div className="App">
-  <Map
-      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-      initialViewState={{
-        longitude: 15,
-        latitude: 47,
-        zoom: 4,
-      
-      }}
-      style={{width: "100vw", height: "100vh"}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    >
-      {pins.map((p) => (
-  <React.Fragment key={p._id}>
-    <Marker longitude={p.long} latitude={p.lat} anchor="bottom" >
-      <LocationOnIcon style={{color: "#5D3FD3"}}
-        onClick={() => handleMarkerClick(p._id)} />
-    </Marker> 
-    {p._id === currentPlaceId && (
-      <Popup key={p._id} longitude={p.long} latitude={p.lat} anchor="left"
-      closeOnClick={false} 
-  onClose={() => setCurrentPlaceId(null)}>
-        <div className='card'>
-          <label>Place</label>
-          <h4 className='place'>{p.title}</h4>
-          <label>Review</label>
-          <p className='desc'>{p.desc}</p>
-          <label>Rating</label>
-          <div>
-            <StarIcon className='star'/>
-            <StarIcon className='star'/>
-            <StarIcon className='star'/>
-            <StarIcon className='star'/>
-            <StarIcon className='star'/>
-          </div>
-          <label>Information:</label>
-          <span className='username'>Created by <b>{p.username}</b></span>
-          <span className='date'>{format(p.createdAt)}</span>
-        </div>
-      </Popup>
-    )}
-  </React.Fragment>
-))}
-      
-    </Map>
+      <Map
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        {...viewState}
+        onMove={(evt) => setViewState(evt.viewState)}
+        style={{ width: '100vw', height: '100vh' }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        onDblClick={handleAddClick}
+        transitionDuration = "200"
+      >
+        {pins.map((p) => (
+          <React.Fragment key={p._id}>
+            <Marker longitude={p.long} latitude={p.lat} anchor="bottom">
+              <LocationOnIcon
+                style={{
+                  color: p.username === currentUser ? 'tomato' : 'slateblue',
+                  cursor: 'pointer'
+                }}
+                onClick={() => handleMarkerClick(p.lat, p.long, p._id)}
+              />
+            </Marker>
+            {p._id === currentPlaceId && (
+              <Popup
+                key={p._id}
+                longitude={p.long}
+                latitude={p.lat}
+                anchor="left"
+                closeOnClick={false}
+                onClose={() => setCurrentPlaceId(null)}
+              >
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="place">{p.title}</h4>
+                  <label>Review</label>
+                  <p className="desc">{p.desc}</p>
+                  <label>Rating</label>
+                  <div>
+                    <StarIcon className="star" />
+                    <StarIcon className="star" />
+                    <StarIcon className="star" />
+                    <StarIcon className="star" />
+                    <StarIcon className="star" />
+                  </div>
+                  <label>Information:</label>
+                  <span className="username">
+                    Created by <b>{p.username}</b>
+                  </span>
+                  <span className="date">{format(p.createdAt)}</span>
+                </div>
+              </Popup>
+            )}
+          </React.Fragment>
+        ))}
+        {newPlace && (
+          <Popup
+            longitude={newPlace.long}
+            latitude={newPlace.lat}
+            anchor="left"
+            closeOnClick={false}
+            onClose={() => setNewPlace(null)}
+          >
+            <div>
+              <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input placeholder='Enter a itle' onChange={(e)=>setTitle(e.target.value)}/>
+                <label>Rewiev</label>
+                <textarea placeholder='Say us something about this place'
+                onChange={(e)=>setDesc(e.target.value)}/>
+                <label>Rating</label>
+                <select onChange={(e)=>setRating(e.target.value)}>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+                <button className='submitButton' type='submit'>Add Pin</button>
+              </form>
+            </div>
+          </Popup>
+        )}
+      </Map>
     </div>
   );
 }
