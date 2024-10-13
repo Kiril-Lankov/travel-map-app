ca99511cff539592b2cd4a9ad33e72ab1578a3f6
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-13 16:07:04 +0300

    Update Register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -14,6 +14,7 @@ export default function Register() {
                     <input type="password" placeholder="password" />
                     <button className="registerBtn">Register</button>
                     <span className="success">Successful.You can login now!</span>
+                    <span className="failure">Something went wrong!</span>
                 </form>   
             </div>
         
